7d9ff9579d209099de7e4ed352a6689f15b5ad1c
Isikyus <aracheldra+github@gmail.com>
2018-06-28 21:44:44 +1000

    Make tests more comprehensive

--- tests/automata/block_test.js
+++ tests/automata/block_test.js
@@ -1,14 +1,30 @@
 var test = require('tape');
 var block = require('../../automata/block.js');
 
-test('flood automaton', function(t) {
+test('flood automaton -- fast', function(t) {
   t.plan(2);
 
-  var flood = block.rules.flood(0.5);
+  var fastFlood = block.rules.flood(1),
+      quicklyBalanced = fastFlood(2, 4, 6, 8),
+      totalAfterBalancing = quicklyBalanced.reduce((a, b) => a + b);
+  t.equal(totalAfterBalancing, 2 + 4 + 6 + 8);
 
-  t.equal(flood(2, 4, 6, 8), [5, 5, 5, 5], 'Distributes evenly where possible');
+  var quickMax = Math.max.apply(Math, quicklyBalanced),
+      quickMin = Math.min.apply(Math, quicklyBalanced),
+      imbalance = quickMax - quickMin;
+  t.ok(imbalance < 3, 'Distributes evenly where fast (got ' + quicklyBalanced + ' -- out by ' + imbalance + ')');
+});
+
+test('flood automaton -- slow', function(t) {
+  t.plan(2);
+
+  var slowFlood = block.rules.flood(0.5),
+      slowSharing = slowFlood(1, 10, 10, 100),
+      totalAfterSharing = slowSharing.reduce((a, b) => a + b);
+  t.equal(totalAfterSharing, 1 + 10 + 10 + 100, 'Preserves total quantity');
   
-  var approxSharing = flood(1, 2, 3, 4);
-  var totalAfterSharing = approxSharing.reduce((a, b) => a + b);
-  t.equal(totalAfterSharing, 1 + 2 + 3 + 4, 'Preserves total quantity');
+  var slowMax = Math.max.apply(Math, slowSharing),
+      slowMin = Math.min.apply(Math, slowSharing),
+      imbalance = slowMax - slowMin;
+  t.ok(imbalance < 75, 'Distributes more evenly than before when slow (got ' + slowSharing + ' -- out by ' + imbalance + ')');
 });
