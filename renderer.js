389fc9e8ce97850b074b94ae0d6afdf1404f0bec
Isikyus <aracheldra+github@gmail.com>
2018-06-28 22:41:14 +1000

    Refactor rendering code
    To support future work on different kinds of renderer.

--- renderer.js
+++ renderer.js
@@ -8,26 +8,41 @@
 module.exports = function(layerColours) {
 
   /**
+   * Renders a cell as a single pixel, at the given scale.
+   */
+  var cellPixel = function(graphicsContext, palette, cell) {
+
+    // Need integer values for CSS "rgb()" function.
+    var rInt = Math.floor(palette.r * cell);
+    var gInt = Math.floor(palette.g * cell);
+    var bInt = Math.floor(palette.b * cell);
+    var colourString = 'rgb(' + rInt + ',' + gInt + ',' + bInt + ')';
+
+    graphicsContext.fillStyle = colourString;
+    graphicsContext.fillRect(0, 0, 1, 1);
+  };
+
+  /**
    * Render a single cellular automaton layer to the canvas.
-   * 
+   *
    * @param graphicsContext used to render to the canvas
+   * @param scale The size of a single cell
    * @param cells The cellular automaton layer to render
    * @param palette The colours to use
+   * @param renderCell Function to render a cell given the context, palette, and cell value.
    */
-  var renderLayer = function(graphicsContext, palette, cells) {
+  var renderLayer = function(graphicsContext, scale, palette, cells, renderCell) {
 
     // TODO: scale to size of canvas.
     for (var i = 0; i < cells.length; i++) {
       for(var j = 0; j < cells[i].length; j++) {
 
-        // Need integer values for CSS "rgb()" function.
-        var rInt = Math.floor(palette.r * cells[i][j]);
-        var gInt = Math.floor(palette.g * cells[i][j]);
-        var bInt = Math.floor(palette.b * cells[i][j]);
-        var colourString = 'rgb(' + rInt + ',' + gInt + ',' + bInt + ')';
+        // TODO: would probably be more efficient to just move around step by step as we draw.
+        graphicsContext.resetTransform();
+        graphicsContext.scale(scale, scale);
+        graphicsContext.translate(i, j);
 
-        graphicsContext.fillStyle = colourString;
-        graphicsContext.fillRect(i, j, 1, 1);
+        renderCell(graphicsContext, palette, cells[i][j]);
       }
     }
   };
@@ -40,8 +55,10 @@ module.exports = function(layerColours) {
   var render = function(canvas, cellValues) {
     for (var layer in layerColours) {
       renderLayer(canvas.getContext('2d'),
+                  1,
                   layerColours[layer],
-                  cellValues[layer]);
+                  cellValues[layer],
+                  cellPixel);
     };
   };
 
