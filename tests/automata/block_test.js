d651d12c325c4139af22b1fe3c0c707f40e6cae0
Isikyus <aracheldra+github@gmail.com>
2018-06-28 22:13:02 +1000

    Make tests more comprehensive

--- tests/automata/block_test.js
+++ tests/automata/block_test.js
@@ -2,7 +2,7 @@ var test = require('tape');
 var block = require('../../automata/block.js');
 
 test('flood automaton -- fast', function(t) {
-  t.plan(2);
+  t.plan(3);
 
   var fastFlood = block.rules.flood(1),
       quicklyBalanced = fastFlood(2, 4, 6, 8),
@@ -13,10 +13,12 @@ test('flood automaton -- fast', function(t) {
       quickMin = Math.min.apply(Math, quicklyBalanced),
       imbalance = quickMax - quickMin;
   t.ok(imbalance < 3, 'Distributes evenly where fast (got ' + quicklyBalanced + ' -- out by ' + imbalance + ')');
+
+  t.deepEqual(fastFlood(0, 0, 0, 0), [0, 0, 0, 0], 'Does nothing to empty space');
 });
 
 test('flood automaton -- slow', function(t) {
-  t.plan(2);
+  t.plan(4);
 
   var slowFlood = block.rules.flood(0.5),
       slowSharing = slowFlood(1, 10, 10, 100),
@@ -27,4 +29,6 @@ test('flood automaton -- slow', function(t) {
       slowMin = Math.min.apply(Math, slowSharing),
       imbalance = slowMax - slowMin;
   t.ok(imbalance < 75, 'Distributes more evenly than before when slow (got ' + slowSharing + ' -- out by ' + imbalance + ')');
+
+  t.deepEqual(slowFlood(0, 0, 0, 0), [0, 0, 0, 0], 'Does nothing to empty space');
 });
